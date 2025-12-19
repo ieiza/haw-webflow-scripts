8d79d21f84adea12c12533f33d50d6d4b7b798cf
ieiza <hello@ikeriza.com>
2025-12-19 09:21:24 -0500

    Update circle-type.js

--- js/circle-type/circle-type.js
+++ js/circle-type/circle-type.js
@@ -1,11 +1,1 @@
 const circleType = new CircleType(document.getElementById('circle-type'));
-
-console.log(circleType.container);
-//=> <div style="position: relative; height: 3.18275em;">...</div>
-
-// Enable the force width option
-circleType.forceWidth(true);
-circleType.forceHeight(true);
-
-console.log(circleType.container);
-//=> <div style="position: relative; height: 3.18275em; width: 12.7473em;">...</div>
