b69e2df46a5a6d6fecb39ffb66194d6ec2895d97
ieiza <hello@ikeriza.com>
2025-12-19 09:06:29 -0500

    Update circle-type.js

--- js/circle-type/circle-type.js
+++ js/circle-type/circle-type.js
@@ -1,6 +1,6 @@
 const circleType = new CircleType(document.getElementById('cta-button--loop'));
 
-circleType.radius(200);
+circleType.radius(100);
 
 // Adds spacing between letters to make a full rotation
 circleType.fullCircle();
